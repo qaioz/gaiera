081c767332c305c0dcd0db24bdb3ce825e1f580d
qaioz <tabatadzegaga@gmail.com>
2025-06-03 14:17:15 +0400

    fix header

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import { Header } from "@/components/header"
+import Header from "@/components/header"
 
 export const metadata = {
   title: "Log In - GaiEra",
